e24076de9ff144e2d8e7e0825c67407b395f8eb0
TaeSeong <ts4683@gmail.com>
2022-03-14 00:10:51 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10172. 개/개.c
+++ 백준/Bronze/10172. 개/개.c
@@ -2,15 +2,10 @@
 
 int main()
 {
-    printf("|\\_/|");
-    printf("\n");
-    printf("|q p|   /}");
-    printf("\n");
-    printf("( 0 )\"\"\"\\");
-    printf("\n");
-    printf("|\"\^\"`    |");
-    printf("\n");
-    printf("||_/=\\\\__|");
-
+    printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("( 0 )\"\"\"\\\n");
+    printf("|\"^\"`    |\n");
+    printf("||_/=\\\\__|\n");
     return 0;
 }
