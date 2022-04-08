8620ddd575c4c17e03956332582233276cd03f80
TaeSeong <ts4683@gmail.com>
2022-04-08 22:06:42 +0900

    [Bronze IV] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int num;
+    scanf("%d",&num);
+    
+    if (num == 0)
+        printf("YONSEI");
+    else if (num == 1)
+        printf("Leading the Way to the Future");
+    return 0;
+}
