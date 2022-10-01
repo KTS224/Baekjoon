a0002a4c3262a38c1b92e58d225c90c9480ba79c
TaeSeong <ts4683@gmail.com>
2022-10-01 21:34:32 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.swift
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.swift
@@ -4,12 +4,10 @@ func solution(_ n:Int) -> Int {
     var answer:Int = 0
     var number: Int = n
     
-    while number >= 10 {
+    while number > 0 {
         answer += number%10
         number = number/10
     }
-    
-    answer += number%10
     
     return answer
 }
