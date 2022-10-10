e3049d863b11f1e886745bcbf8f96897ee8a049a
TaeSeong <ts4683@gmail.com>
2022-10-10 18:17:27 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.02 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.swift
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.swift
@@ -1,26 +1,15 @@
 func solution(_ phoneNumber: String) -> String {
-    var answer: String = phoneNumber
+    var s: String = phoneNumber
+
+    s.removeFirst(phoneNumber.count - 4)
     
-    //phoneNumber의 길이-4만큼 answer을 제거 후
-    answer.removeFirst(phoneNumber.count - 4)
-    
-    //길이가 4인 경우는 제외
-    guard phoneNumber.count == 4 else {
-        //answer에 *을 제거한만큼 이어붙힌다. 
-        for _ in 1 ... phoneNumber.count - 4 {
-            answer = "*" + answer
-        }
-        
-        return answer
+    guard phoneNumber.count > 4 else {
+        return s
     }
     
-// if 문 풀이
-    // if phoneNumber.count == 4 {
-    //     return answer
-    // } else {
-    //     for _ in 1 ... phoneNumber.count - 4 {
-    //         answer = "*" + answer
-    //     }
-    // }
-    return answer
+    for _ in 1 ... phoneNumber.count - 4 {
+        s = "*" + s
+    }
+        
+    return s
 }
