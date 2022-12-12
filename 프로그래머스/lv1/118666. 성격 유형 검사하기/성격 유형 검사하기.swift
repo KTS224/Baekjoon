feaa089025a3fa7ba120a8f5ee763490397a6972
김태성 <ts4683@gmail.com>
2022-12-12 10:20:14 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 6.01 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.swift
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 func solution(_ survey:[String], _ choices:[Int]) -> String {
-    var choices = choices.map { $0 - 4 } // 초이스를 점수화한다
+    var choices = choices.map { $0 - 4 }
     var result: [[String]] = [[""], ["R"], [""], ["T"], [""], ["C"], [""], ["F"], [""], ["J"], [""], ["M"], [""], ["A"], [""], ["N"]]
 
     for (survey, score) in zip(survey, choices) {
@@ -13,19 +13,13 @@ func solution(_ survey:[String], _ choices:[Int]) -> String {
     }
 
     var score: [Int] = []
-
     for i in stride(from: 0, to: result.count, by: 2) {
-        var temp = 0
-        temp = result[i][0].components(separatedBy: " ").reduce(0) { (Int($0) ?? 0) + (Int($1) ?? 0) }
-        score.append(temp)
+        score.append(result[i][0].components(separatedBy: " ").reduce(0) { (Int($0) ?? 0) + (Int($1) ?? 0) })
     }
 
     var answer = ""
-
     for i in stride(from: 0, to: score.count, by: 2) {
-        print("\(answer) += \(score[i]) >= \(score[i+1]) ? \(result[i*2+1][0]) : \(result[i*2+3][0])")
         answer += score[i] >= score[i+1] ? result[i*2+1][0] : result[i*2+3][0]
     }
-    
     return answer
 }
