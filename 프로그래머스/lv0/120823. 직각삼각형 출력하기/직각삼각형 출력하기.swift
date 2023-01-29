b5984d2a02b8f911d178dce3c40156d31405ea1f
김태성 <ts4683@gmail.com>
2023-01-29 18:54:03 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 5.74 ms, Memory: 14.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+
+for i in 1...n[0] {
+    for k in 1...i {
+        print("*", terminator: "")
+    }
+    print("")
+}
