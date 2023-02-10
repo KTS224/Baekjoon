6f565198c93dfcba541170874d58ed5fa59eee3a
김태성 <ts4683@gmail.com>
2023-02-11 01:33:57 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.swift
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.swift
@@ -2,8 +2,7 @@ import Foundation
 
 func solution(_ my_string:String, _ num1:Int, _ num2:Int) -> String {
     var arr = Array(my_string)
-    var temp = arr[num1]
-    arr[num1] = arr[num2]
-    arr[num2] = temp
+    arr.swapAt(num1, num2)
+    
     return String(arr)
 }
